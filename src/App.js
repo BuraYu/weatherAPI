ce881dbaca731423ac6cddbc21b4b8e54481a40b
Burak Yüksel <buriii@hotmail.de>
2023-12-02 23:03:04 +0100

    add skeleton css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - On it
-        </a>
-      </header>
+    <div className="container">
+      <h1>0°</h1>
+      <h2>chilly</h2>
     </div>
   );
 }
